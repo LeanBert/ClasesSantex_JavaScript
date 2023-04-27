ea80d3a61d8e0342d8d2a3d7c0d364770d656783
LeanBert <leanbertero@hotmail.com>
2023-04-27 01:29:49 -0300

    primeros pasos en js

--- JavaScript/primerClase.js
+++ JavaScript/primerClase.js
@@ -1,1 +1,9 @@
-https://drive.google.com/file/d/1JYkd5lm7ChVdnEPn5STfUBcEZZp3kP6h/view
+let nombre = "Leandro";
+let apellido = "Perez";
+let edad = null;
+let direccion;
+
+console.log("Hola, mi nombre es:" , " " , nombre );
+console.log("Hola, mi apellido es:" + " " +    apellido );
+console.log("Mi edad es de:" + edad);
+console.log("Mi direccion es:" + " " + direccion);
