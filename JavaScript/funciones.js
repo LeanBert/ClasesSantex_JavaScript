9c9808099dc2ef3c0193b39469a057f839a1be4f
LeanBert <leanbertero@hotmail.com>
2023-04-27 04:05:47 -0300

    funciones

---
+++ JavaScript/funciones.js
@@ -0,0 +1,7 @@
+//funcion saludar  -  dentron de la funcion contiene hola!  - mensaje que se muestra en pantalla
+function saludar(){
+    console.log("Hola!");
+}
+
+//para invocar o llamar a la funcion
+saludar();
