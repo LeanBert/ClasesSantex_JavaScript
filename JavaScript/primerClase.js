44ed3cd5f5906891dc5dc6382531f247c8962556
LeanBert <leanbertero@hotmail.com>
2023-04-27 03:57:12 -0300

    variables let

--- JavaScript/primerClase.js
+++ JavaScript/primerClase.js
@@ -1,9 +1,36 @@
-let nombre = "Leandro";
-let apellido = "Perez";
-let edad = null;
-let direccion;
 
-console.log("Hola, mi nombre es:" , " " , nombre );
-console.log("Hola, mi apellido es:" + " " +    apellido );
-console.log("Mi edad es de:" + edad);
-console.log("Mi direccion es:" + " " + direccion);
+
+// let nombre = "Leandro";
+// let apellido = "Perez";
+// let edad = null;
+// let direccion;
+
+// console.log("Hola, mi nombre es:" , " " , nombre );
+// console.log("Hola, mi apellido es:" + " " +    apellido );
+// console.log("Mi edad es de:" + edad);
+// console.log("Mi direccion es:" + " " + direccion);
+
+
+//operadores de comparacion
+
+// console.log(2 < 10);
+// console.log(12 == 12); 
+// console.log(2 == "2"); // dos iguales compara la equidad entre dos datos 
+// console.log(2 === "2"); //tres iguales comparan el tipo de datos
+
+// usar triple igual
+
+//condicionales
+
+let numero = 1;
+let condicion = true;
+console.log("antes del while");
+
+//mientras  
+while(numero < 10) {
+    numero.log(numero);
+    //esto se repite
+    numero = false;
+}
+
+console.log("despues del while");
