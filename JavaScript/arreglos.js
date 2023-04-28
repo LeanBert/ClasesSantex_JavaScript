07bf5918f4ce32d0c5bfd0a0522ca8e3e389864d
LeanBert <leanbertero@hotmail.com>
2023-04-28 15:15:52 -0300

    arreglos

---
+++ JavaScript/arreglos.js
@@ -0,0 +1,17 @@
+//arreglos = permite guardar multiples elementos dentro de una sola variable
+// puede ser ccedido por medio de un indice
+//el indice de los arrays inicia en cero
+//puede contener valores de tipo mixtos
+
+/*Los arreglos van con corchetes (IMPORTANTE)*/
+//el contenido del arreglo debe estar separado por coma (no olvidar) 
+//puede ir cualquier tipo de dato dentro de un array
+
+const listadeAlumnos = [
+    "pepe",
+    "pepe2",
+    "pepe3",
+    "pepe4", 
+];
+
+console.log(listadeAlumnos[2]); 
--- JavaScript/herencia.js
+++ JavaScript/herencia.js
@@ -1,1 +1,25 @@
-// continuar con herencia clase 2 Js -  continuar viendo desde 1hs 58 min
+
+//Herencia = la Herencia es alguna caracteristica que puede tomar un objeto de otro objeto 
+
+class Persona{
+    nombre;
+    apellido;
+
+    constructor (nombreInicial,apellidoInicial){
+        this.nombre = nombreInicial;
+        this.nombre = apellidoInicial;
+    }
+
+    presentarse(){
+        console.log('Hola, mi nombre es ' , this.nombre, '' , this.apellido);
+    }
+}
+
+
+/**/
+class Alumno extends Persona{
+    conlegio = 'Xacademy';
+}
+
+const alumno = new Alumno(); 
+console.log(alumno);
